7be4329c9826cddb6bedac2a7886675697a15afc
Your Name <you@example.com>
2025-04-28 13:51:19 +0530

    skkk

---
+++ operators.py
@@ -0,0 +1,154 @@
+# 10 + 20
+# 10 = operands
+# + = operators
+
+
+# 1.Arithmetic operators : +,-,*,/,
+
+# % = reminder
+
+# 2.logical operators = and,or,not
+# 3.compression operators = !=,==,<,>
+# 4.assigment oprators = "=",a = 10,b = 20
+
+# Python Conditions and If statements
+# Python supports the usual logical conditions from mathematics:
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+
+# control statements:
+    
+#     control statement are those statementss which are control the flow of Execution of a  program.
+
+# conditional statements
+        #  if
+        #  else
+        #  elif
+        
+# a = 33
+# b = 200
+# if b > a:
+#   print("b is greater than a")
+
+# output:b is greater than a
+
+
+# sytax :
+#     if condition:
+#         if 10==10:
+#             print("these values are equal")
+            
+#             o/p these value is true
+
+
+# else:
+#      
+#       if 10==20:
+#          print("these values are equal")
+#       else:
+#         print("they are not equal")  
+
+
+# elif:
+#        when conditions are two or more than 2 conditions
+
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+
+# if,elif,else----->
+
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+# output: a greater than b
+
+
+
+
+1.#calculator
+
+# print("**** calculator****")
+# fnum = int(input("enter first number"))
+# nnum = int(input("enter second number"))
+
+# print("""::::see your choice::::
+#          1) addition
+#          2) substraction
+#          3) multiplication
+#          4) division
+# """)
+# choice = int(input("enter what you want to do :"))
+# if choice==1:
+#   x =fnum+nnum
+#   print(f"the sum of {fnum} and {nnum}is:",x)
+# elif choice ==2:
+#   x =fnum-nnum
+#   print(f"the subtracrion of{fnum}and {nnum}is:",x)
+# elif choice==3:
+#   x =fnum*nnum
+#   print(f"the multipliction of{fnum}and{nnum} is:",x)
+# elif choice==4:
+#   x= fnum/Num
+#   print(f"the division of{fnum}and {nnum} is:",x)
+# else:
+#   print("enter valid choice")
+
+
+
+
+
+2.# print("****voting system****")
+# age=int(input("enter your age"))
+# if age >18:
+#   print("eligible fot vote")
+# else:
+#   ("not eligible for vote")
+
+
+
+
+3.# first_number=int(input("enter first number"))
+# second_number=int(input("enter secound number"))
+# solution = (first_number + second_number)
+# print(solution)
+
+
+# 4.class animal:
+#   def speak (self):
+#     print ("animal sound")
+# class dog (animal):
+#   def bark(self):
+#     print("woof")
+# dog=dog()
+# dog.speak()
+# dog.bark()
+
+
+
+        
+          
+
+
+
+
+
+
+
