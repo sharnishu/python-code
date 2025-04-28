9a0c6943c25f6494b4f38dbaa6661c8d6de7bfc6
Your Name <you@example.com>
2025-04-28 14:00:39 +0530

    skkk

---
+++ this is a car.py
@@ -0,0 +1,40 @@
+# zx="this is a car"
+#  print(len(zx))
+
+# # OUTPUT:13
+
+
+# a=91923900120300021
+# b=str(a)
+# print(len(b))
+
+# # OUTPUT:17
+      
+
+
+# a = "78"
+# b = 50
+# c = int(a)
+# x = b+c
+# print(x)
+
+# # OUTPUT:128
+
+# print("****students score****")
+# marks=int(input("enter your marks"))
+# if marks >= 90 <= 100:
+#     print("grade a")
+# elif marks >=80 < 89:
+#     print("grade b")
+# elif marks <=70 <=79:
+#     print("grade c")
+# elif marks <= 60 <69:
+#     print ("grade d")
+# else:
+#     print("grade e")
+    
+# # OUTPUT:   
+# # ****students score****
+# # enter your marks77
+# # grade e
+
